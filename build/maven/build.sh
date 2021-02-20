375f0cbfa071d514c606b9df44dcfd3743c88a07
kangta123 <kang7213@gmail.com>
2021-02-20 18:32:49 +0800

    combined maven build script

--- build/maven/build.sh
+++ build/maven/build.sh
@@ -9,6 +9,13 @@ check(){
   echo "=@@$2 ${buildRet} $3@@@"
 }
 
+init_deps(){
+  if [ -z "$(ls -A $1)" ]; then
+    echo "Maven repo Empty, Loading dependencies..."
+    mvn dependency:resolve -f app/
+  fi
+}
+
 do_package(){
     mvn install -Dfindbugs.skip -Dcheckstyle.skip -Dpmd.skip=true -Denforcer.skip -Dmaven.javadoc.skip -Dmaven.test.skip.exec -Dlicense.skip=true -Drat.skip=true  -DskipTests -Dsonar.skip=true -Dpmd.skip=true -f $1
 }
@@ -63,7 +70,7 @@ build_docker(){
 
   mkdir docker 2>/dev/null
   docker  build \
-        --build-arg JAR_FILE=${JAR_FILE}  \
+        --build-arg OUTPUT=${JAR_FILE}  \
         -t ${IMAGE_NAME}:${TAG} . -f - < ${tmp}
 
   check $? "DockerBuild" ${IMAGE_NAME}
@@ -73,30 +80,56 @@ build_docker(){
 
 }
 
+git_pull(){
+  check 0 "Start"
+  rm -fr app
+  git clone --depth 1 -b ${REMOTE_BRANCH} ${GIT_URL}  app  && cd app
+  check $? "GitClone"
+}
 
-check 0 "Start"
-rm -fr app
-git clone --depth 1 -b ${REMOTE_BRANCH} ${GIT_URL}  app  && cd app
-check $? "GitClone"
+build_jar(){
+    unzip -l ${f} |grep "application.*\.properties\|application.*\.yml\|bootstrap.*\.yml\|bootstrap.*\.properties"   > /dev/null
+    if [ $? -eq "0" ]; then
+     echo "build file ${f} `pwd`"
+      build_docker ${f}
+    fi
+}
 
-size=`find . -name "pom.xml"|wc -l`
-if [ ${size} -eq '0' ]; then
-  echo "Can not found file pom.xml"
-  exit
-fi
+build_war(){
 
-test -z $SUB_PATH && package || package_assign_project
+  target=`find . -name "*.war" | head -1`
+  echo "target is ${target}"
+  targetFolder=`dirname ${target}`
+  cd ${targetFolder}
 
-find . -regex ".*/target/[^/]*\.*[j|w]ar" |grep -v "sources\.jar"
+  targetFile=`ls *.war | head -1`
 
-for f in `find . -regex ".*/target/[^/]*\.*[j|w]ar" |grep -v "sources\.jar"`
-do
-  echo "build source ${f}"
-  unzip -l ${f} |grep "application.*\.properties\|application.*\.yml\|bootstrap.*\.yml\|bootstrap.*\.properties"   > /dev/null
-  if [ $? -eq "0" ]; then
-    echo "build file ${f} `pwd`"
-    build_docker ${f}
+  build_docker "${targetFile%.*}"
+
+}
+build(){
+  size=`find . -name "pom.xml"|wc -l`
+  if [ ${size} -eq '0' ]; then
+   echo "Can not found file pom.xml"
+    exit
   fi
-done
+
+  test -z $SUB_PATH && package || package_assign_project
+
+
+  for f in `find . -regex ".*/target/[^/]*\.*[j|w]ar" |grep -v "sources\.jar"`
+  do
+    echo "build source ${f}"
+
+    echo $f | grep -qE ".war$" && build_war $f
+    echo $f | grep -qE ".jar$" && build_jar $f 
+  done
+}
+
+git_pull
+
+init_deps
+
+build
 
 check 0 "End"
